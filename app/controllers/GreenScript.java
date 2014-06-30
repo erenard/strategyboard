1f9468e9e56b27d7f4f0713fba5143b00fcc9f70
erenard <timmy469@gmail.com>
2014-06-30 22:17:42 +0200

    OpenTTD 1.4.1 Suppression de Greenscript

--- app/controllers/GreenScript.java
+++
@@ -1,8 +0,0 @@
-package controllers;
-
-import play.mvc.With;
-import controllers.greenscript.Configurator;
-
-@With({Secure.class, Configurator.class})
-@Check(Profile.ADMINISTRATOR)
-public class GreenScript extends Configurator {}
