8f50d73e042d16ead41a1f8df784540490b3f631
Éric Renard <timmy469@gmail.com>
2022-09-05 04:41:35 +0200

    Cache big request results

--- app/controllers/openttd/Openttd.java
+++ app/controllers/openttd/Openttd.java
@@ -14,6 +14,7 @@ import models.leaderboard.PlayerScore;
 import models.leaderboard.Scenario;
 import models.openttd.OpenttdServer;
 import models.openttd.RobotConfiguration;
+import play.cache.Cache;
 import play.mvc.With;
 
 @With(Profile.class)
@@ -27,13 +28,41 @@ public class Openttd extends Application {
 		OpenttdServer steamStatus = OpenttdServer.getStatus(RobotConfiguration.STEAM);
 		OpenttdServer toylandStatus = OpenttdServer.getStatus(RobotConfiguration.TOYLAND);
 
-		List<PlayerScore> playerScores = PlayedGameScore.getLastMonthPlayerScores(1, 10);
-		List<PlayedGame> lastPlayedGames = PlayedGame.getLastPlayedGames();
-		List<PlayedGame> bestArticGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.Artic);
-		List<PlayedGame> bestDeserticGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.Desertic);
-		List<PlayedGame> bestHighSpeedGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.HighSpeed);
-		List<PlayedGame> bestSteamGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.Steam);
-		List<PlayedGame> bestToylandGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.Toyland);
+		List<PlayerScore> playerScores = Cache.get("lastMonthPlayerScores", List.class);
+		if(playerScores == null) {
+			playerScores = PlayedGameScore.getLastMonthPlayerScores(1, 10);
+			Cache.set("lastMonthPlayerScores", playerScores, "5s");
+		}
+		List<PlayedGame> lastPlayedGames = Cache.get("lastPlayedGames", List.class);
+		if(lastPlayedGames == null) {
+			lastPlayedGames = PlayedGame.getLastPlayedGames();
+			Cache.set("lastPlayedGames", lastPlayedGames, "5s");
+		}
+		List<PlayerScore> bestArticGames = Cache.get("bestArticGames", List.class);
+		if(bestArticGames == null) {
+			bestArticGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.Artic);
+			Cache.set("bestArticGames", bestArticGames, "5s");
+		}
+		List<PlayerScore> bestDeserticGames = Cache.get("bestDeserticGames", List.class);
+		if(bestDeserticGames == null) {
+			bestDeserticGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.Desertic);
+			Cache.set("bestDeserticGames", bestDeserticGames, "5s");
+		}
+		List<PlayerScore> bestHighSpeedGames = Cache.get("bestHighSpeedGames", List.class);
+		if(bestHighSpeedGames == null) {
+			bestHighSpeedGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.HighSpeed);
+			Cache.set("bestHighSpeedGames", bestHighSpeedGames, "5s");
+		}
+		List<PlayerScore> bestSteamGames = Cache.get("bestSteamGames", List.class);
+		if(bestSteamGames == null) {
+			bestSteamGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.Steam);
+			Cache.set("bestSteamGames", bestSteamGames, "5s");
+		}
+		List<PlayerScore> bestToylandGames = Cache.get("bestToylandGames", List.class);
+		if(bestToylandGames == null) {
+			bestToylandGames = PlayedGame.getBestPlayedGamesForScenarioId(Scenario.Id.Toyland);
+			Cache.set("bestToylandGames", bestToylandGames, "5s");
+		}
 		render(
 				articStatus,
                 deserticStatus,
--- app/controllers/openttd/Scores.java
+++ app/controllers/openttd/Scores.java
@@ -9,46 +9,50 @@ import controllers.Profile;
 
 import models.leaderboard.PlayedGameScore;
 import models.leaderboard.PlayerScore;
+import play.cache.Cache;
 import play.mvc.With;
 import tasks.ExportScoresCsvGenerator;
 
 @With(Profile.class)
 public class Scores extends Application {
 
-	public static String period_last_month = "last_month";
-	public static String period_forever = "forever";
-	
-	@Check(value = {Profile.VIEWER})
-	public static void index() {
-		show(period_last_month, 1);
-	}
-	
-	@Check(value = {Profile.VIEWER})
-	public static void show(String period, Integer page) {
-		long nbScores = 0;
-		List<PlayerScore> playerScores = null;
-		Calendar from = Calendar.getInstance();
-		Calendar to = Calendar.getInstance();
-		if(period != null && period_forever.equals(period)) {
-			from.setTimeInMillis(0);
-		} else {
-			period = period_last_month;
-			from.add(Calendar.MONTH, -1);
-		}
-		playerScores = PlayedGameScore.getPlayerScores(from, to, page == null ? 1 : page, pageSize);
-		nbScores = PlayedGameScore.count(from, to);
-		render(playerScores, nbScores, period, page);
-	}
+    public static String period_last_month = "last_month";
+    public static String period_forever = "forever";
 
-	@Check(value = {Profile.MODERATOR})
-	public static void exportAsCSV() {
-		List<PlayedGameScore> scores = PlayedGameScore.all().fetch(10);
-		ExportScoresCsvGenerator generator = new ExportScoresCsvGenerator(scores);
-		response.contentType = "text/csv";
-		while(generator.hasMoreData()) {
-			String someCsvData = await(generator.nextDataChunk());
-			response.writeChunk(someCsvData);
-		}
-	}
+    @Check(value = {Profile.VIEWER})
+    public static void index() {
+        show(period_last_month, 1);
+    }
+
+    @Check(value = {Profile.VIEWER})
+    public static void show(String period, Integer page) {
+        long nbScores = 0;
+        Calendar from = Calendar.getInstance();
+        Calendar to = Calendar.getInstance();
+        if (period != null && period_forever.equals(period)) {
+            from.setTimeInMillis(0);
+        } else {
+            period = period_last_month;
+            from.add(Calendar.MONTH, -1);
+        }
+        List<PlayerScore> playerScores = Cache.get("playerScores_" + period + "_" + page, List.class);
+        if (playerScores == null) {
+            playerScores = PlayedGameScore.getPlayerScores(from, to, page == null ? 1 : page, pageSize);
+            Cache.set("playerScores_" + period + "_" + page, playerScores, "5s");
+        }
+        nbScores = PlayedGameScore.count(from, to);
+        render(playerScores, nbScores, period, page);
+    }
+
+    @Check(value = {Profile.MODERATOR})
+    public static void exportAsCSV() {
+        List<PlayedGameScore> scores = PlayedGameScore.all().fetch(10);
+        ExportScoresCsvGenerator generator = new ExportScoresCsvGenerator(scores);
+        response.contentType = "text/csv";
+        while (generator.hasMoreData()) {
+            String someCsvData = await(generator.nextDataChunk());
+            response.writeChunk(someCsvData);
+        }
+    }
 
 }
