d5e66dd159694b3a54ffd5529a24588c15218489
Éric Renard <timmy469@gmail.com>
2022-09-05 05:52:51 +0200

    feat one best score by player

--- app/models/leaderboard/PlayedGame.java
+++ app/models/leaderboard/PlayedGame.java
@@ -11,29 +11,31 @@ import play.db.jpa.Model;
 @Entity
 public class PlayedGame extends Model {
 
-   @ManyToOne
-   public Scenario scenario;
+    @ManyToOne
+    public Scenario scenario;
 
-   @Required
-   @As("yyyy-MM-dd kk:mm")
-   public Date begin;
-   
-   @Required
-   @As("yyyy-MM-dd kk:mm")
-   public Date end;
-   
-   @OneToMany(cascade = CascadeType.ALL, mappedBy = "playedGame")
-   public List<PlayedGameScore> scores = new ArrayList<PlayedGameScore>();
-   
-   public PlayerScore getWinner() {
-	   return PlayedGameScore.getWinner(this);
-   }
-   
-   public static List<PlayedGame> getLastPlayedGames() {
-	   return find("select p from PlayedGame p order by p.end desc").fetch(10);
-   }
-   
-   public static List<PlayedGame> getBestPlayedGamesForScenarioId(Scenario.Id scenarioId) {
-	   return find("select p.playedGame from PlayedGameScore p where p.playedGame.scenario = " + scenarioId.longValue() + " order by p.achivement desc").fetch(10);
-   }
+    @Required
+    @As("yyyy-MM-dd kk:mm")
+    public Date begin;
+
+    @Required
+    @As("yyyy-MM-dd kk:mm")
+    public Date end;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "playedGame")
+    public List<PlayedGameScore> scores = new ArrayList<PlayedGameScore>();
+
+    public PlayerScore getWinner() {
+        return PlayedGameScore.getWinner(this);
+    }
+
+    public static List<PlayedGame> getLastPlayedGames() {
+        return find("select p from PlayedGame p order by p.end desc").fetch(10);
+    }
+
+    public static List<PlayedGame> getBestPlayedGamesForScenarioId(Scenario.Id scenarioId) {
+        return find("select s.playedGame, max(s.achivement) as ma, s.user from PlayedGameScore s "
+            + "where s.playedGame.scenario.id = " + scenarioId.longValue() + " "
+            + "group by s.user order by ma desc").fetch(10);
+    }
 }
